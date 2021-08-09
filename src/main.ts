afe3bd28b86dcd35314f461ab8963fb40b9f7a44
kinglim3 <limkingfai@gmail.com>
2021-08-09 23:44:22 +0800

    PoC

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
+import { Container, Inject, InjectValue, Singleton } from 'typescript-ioc'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+Container.bindName('str').to('abc');
+
+@Singleton
+class BeanA {
+	@InjectValue('str')
+	public str?: string;
+}
+
+@Singleton
+class BeanB {
+
+	@Inject
+	private beanA?: BeanA;
+
+}
+
+
+console.log(new BeanB());
