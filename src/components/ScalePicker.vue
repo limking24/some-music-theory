45ddfa2b80b6b19b7ee7b92f13fee55407af16ba
Lim King Fai <king-fai.lim@varmeego.com>
2021-08-12 18:24:08 +0800

    Scale picker draft (unfinished)

---
+++ src/components/ScalePicker.vue
@@ -0,0 +1,26 @@
+<template>
+	<form>
+		<label for="scale">Scale</label>
+		<select v-model="type" id="scale">
+			<option>Major</option>
+			<option>Minor</option>
+		</select>
+	</form>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+import { Prop, PropSync } from 'vue-property-decorator';
+
+export default class ScalePicker extends Vue {
+
+//https://stackoverflow.com/questions/40915436/vuejs-update-parent-data-from-child-component
+	@PropSync('syncedModel', {required: true})
+	private model!: String;
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+	<scale-picker :type.sync="type"></scale-picker>
+	{{scale}}
   </div>
 </template>
+
+<script lang="ts">
+import ScalePicker from '@/components/ScalePicker.vue';
+import Scale from '@/models/scale';
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: { ScalePicker }
+})
+export default class About extends Vue {
+
+	private scale = new Scale('Major', 'Ionian', 'C');
+
+	public test() {
+		console.log('test');
+	}
+
+}
+</script>
