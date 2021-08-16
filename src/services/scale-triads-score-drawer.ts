0c564c5b6491404dbf58cbeb9aa8bb0c1b1fbf13
Lim King Fai <king-fai.lim@varmeego.com>
2021-08-16 18:11:38 +0800

    Refactor code

--- src/services/scale-triads-score-drawer.ts
+++ src/services/scale-triads-score-drawer.ts
@@ -19,44 +19,23 @@ export class VexFlowScaleTriadsScoreDrawer extends ScaleTriadsScoreDrawer {
 
 	draw(scale: Scale): void {
 		let triads = '(A3 C4 E4)/w, (B3 D4 F4), (C4 E4 G4), (D4 F4 A4), (E4 G4 B4), (F4 A4 C5), (G4 B4 D5), (A4 C5 E5), (B4 D5 F5), (C5 E5 G5), (D5 F5 A5)';
+		let chordNames = ['Am', 'Bdim', 'C', 'D', 'Em', 'F', 'G', 'Am', 'Bdim', 'C', 'D'];
+
 		let vf = new Vex.Flow.Factory({renderer: {elementId: this.elementId, width: 715}});
-		
 		let score = vf.EasyScore();
 		let system = vf.System({width: 700});
-		
-		let chordNames = [
-			new Vex.Flow.TextNote({text: 'Am', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'Bdim', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'C', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'D', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'Em', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'F', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'G', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'Am', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'Bdim', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'C', duration: 'w'}),
-			new Vex.Flow.TextNote({text: 'D', duration: 'w'}),
-		];
-
-		chordNames.forEach(name => name
-									.setJustification(Vex.Flow.TextNote.Justification.CENTER)
-									.setLine(12));
-
-		let voices = [
-			score.voice(score.notes(triads), {time: '11/1'}),
-			score.voice(chordNames, {time: '11/1'})
-		];
-
+		let chordTextNotes = chordNames.map(chord => new Vex.Flow.TextNote({text: chord, duration: 'w'})
+															.setJustification(Vex.Flow.TextNote.Justification.CENTER)
+															.setLine(12));
 		system
 			.addStave({
-				voices,
-				options: {
-					x_shift: 10
-				}
+				voices: [
+					score.voice(score.notes(triads), {time: '11/1'}),
+					score.voice(chordTextNotes, {time: '11/1'})
+				]
 			})
 			.addClef('treble')
 			.addKeySignature('C');
-
 		vf.draw();
 	}
 
