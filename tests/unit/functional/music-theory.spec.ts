d2a23a95b256cfb768f97eaefcdab17f93d7296a
kinglim3 <limkingfai@gmail.com>
2021-08-20 00:11:37 +0800

    Update test unit

--- tests/unit/functional/music-theory.spec.ts
+++ tests/unit/functional/music-theory.spec.ts
@@ -5,23 +5,23 @@ import { getScaleInfo, getScaleTriadNames } from '@/functional/music-theory';
 describe('functional/music-theory', () => {
 
 	it('getScale', () => {
-		expect(getScaleInfo(ScaleModel.create('Major', 'Ionian', 'C')).name).to.equal('C major');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Dorian', 'C')).name).to.equal('C dorian');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Phrygian', 'C')).name).to.equal('C phrygian');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Lydian', 'C')).name).to.equal('C lydian');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Mixolydian', 'C')).name).to.equal('C mixolydian');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Aeolian', 'C')).name).to.equal('C aeolian');
-		expect(getScaleInfo(ScaleModel.create('Major', 'Locrian', 'C')).name).to.equal('C locrian');
-		expect(getScaleInfo(ScaleModel.create('Minor', 'Natural', 'C')).name).to.equal('C aeolian');
-		expect(getScaleInfo(ScaleModel.create('Minor', 'Harmonic', 'C')).name).to.equal('C harmonic minor');
-		expect(getScaleInfo(ScaleModel.create('Minor', 'Melodic', 'C')).name).to.equal('C melodic minor');
+		expect(getScaleInfo(ScaleModel.create('major', 'ionian', 'c')).name).to.equal('C major');
+		expect(getScaleInfo(ScaleModel.create('major', 'dorian', 'c')).name).to.equal('C dorian');
+		expect(getScaleInfo(ScaleModel.create('major', 'phrygian', 'c')).name).to.equal('C phrygian');
+		expect(getScaleInfo(ScaleModel.create('major', 'lydian', 'c')).name).to.equal('C lydian');
+		expect(getScaleInfo(ScaleModel.create('major', 'mixolydian', 'c')).name).to.equal('C mixolydian');
+		expect(getScaleInfo(ScaleModel.create('major', 'aeolian', 'c')).name).to.equal('C aeolian');
+		expect(getScaleInfo(ScaleModel.create('major', 'locrian', 'c')).name).to.equal('C locrian');
+		expect(getScaleInfo(ScaleModel.create('minor', 'natural', 'c')).name).to.equal('C aeolian');
+		expect(getScaleInfo(ScaleModel.create('minor', 'harmonic', 'c')).name).to.equal('C harmonic minor');
+		expect(getScaleInfo(ScaleModel.create('minor', 'melodic', 'c')).name).to.equal('C melodic minor');
 	});
 
 	it('getScaleTriadNames', () => {
-		expect(getScaleTriadNames(ScaleModel.create('Major', 'Ionian', 'C'))).to.deep.equal(['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim']);
-		expect(getScaleTriadNames(ScaleModel.create('Minor', 'Natural', 'C'))).to.deep.equal(['Cm', 'Ddim', 'Eb', 'Fm', 'Gm', 'Ab', 'Bb']);
-		expect(getScaleTriadNames(ScaleModel.create('Minor', 'Harmonic', 'C'))).to.deep.equal(['Cm', 'Ddim', 'Ebaug', 'Fm', 'G', 'Ab', 'Bdim']);
-		expect(getScaleTriadNames(ScaleModel.create('Minor', 'Melodic', 'C'))).to.deep.equal(['Cm', 'Dm', 'Ebaug', 'F', 'G', 'Adim', 'Bdim']);
+		expect(getScaleTriadNames(ScaleModel.create('major', 'ionian', 'c'))).to.deep.equal(['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim']);
+		expect(getScaleTriadNames(ScaleModel.create('minor', 'natural', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Eb', 'Fm', 'Gm', 'Ab', 'Bb']);
+		expect(getScaleTriadNames(ScaleModel.create('minor', 'harmonic', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Ebaug', 'Fm', 'G', 'Ab', 'Bdim']);
+		expect(getScaleTriadNames(ScaleModel.create('minor', 'melodic', 'c'))).to.deep.equal(['Cm', 'Dm', 'Ebaug', 'F', 'G', 'Adim', 'Bdim']);
 	});
 
 });
