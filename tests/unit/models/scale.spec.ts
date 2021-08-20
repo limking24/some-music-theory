f0ddaefb0edbe1d3657fe3bc60ddae9c1cb6369b
Lim King Fai <king-fai.lim@varmeego.com>
2021-08-20 16:04:57 +0800

    Add and update unit test

--- tests/unit/models/scale.spec.ts
+++ tests/unit/models/scale.spec.ts
@@ -8,8 +8,8 @@ describe('model/scale', () => {
 		it('create', () => {
 			let cIonian = new Scale('major', 'ionian', 'c');
 			let dIonian = new Scale('major', 'ionian', 'd');
-			let cNaturalMinor = new Scale('minor', 'natural', 'c');
-			let gFlatHarmonicMinor = new Scale('minor', 'harmonic', 'g-flat');
+			let aNaturalMinor = new Scale('minor', 'natural', 'a');
+			let bFlatHarmonicMinor = new Scale('minor', 'harmonic', 'b-flat');
 	
 			expect(Scale.create('', '', '')).to.deep.equal(cIonian);
 			expect(Scale.create('', '', '')).to.deep.equal(cIonian);
@@ -21,20 +21,20 @@ describe('model/scale', () => {
 			expect(Scale.create('', '', 'z')).to.deep.equal(cIonian);
 	
 			expect(Scale.create('MAJOR', 'NATURAL', '')).to.deep.equal(cIonian);
-			expect(Scale.create('mInOr', 'IoNiAn', '')).to.deep.equal(cNaturalMinor);
+			expect(Scale.create('mInOr', 'IoNiAn', '')).to.deep.equal(aNaturalMinor);
 	
-			expect(Scale.create('minor', '', '')).to.deep.equal(cNaturalMinor);
-			expect(Scale.create('', 'natural', '')).to.deep.equal(cNaturalMinor);
-			expect(Scale.create('minor', 'natural', '')).to.deep.equal(cNaturalMinor);
-			expect(Scale.create('Minor', 'Natural', 'C')).to.deep.equal(cNaturalMinor);
+			expect(Scale.create('minor', '', '')).to.deep.equal(aNaturalMinor);
+			expect(Scale.create('', 'natural', '')).to.deep.equal(aNaturalMinor);
+			expect(Scale.create('minor', 'natural', '')).to.deep.equal(aNaturalMinor);
+			expect(Scale.create('Minor', 'Natural', 'A')).to.deep.equal(aNaturalMinor);
 	
 			expect(Scale.create('major', '', '')).to.deep.equal(cIonian);
 			expect(Scale.create('', 'ionian', '')).to.deep.equal(cIonian);
 			expect(Scale.create('major', 'ionian', '')).to.deep.equal(cIonian);
 			expect(Scale.create('Major', 'Ionian', 'C')).to.deep.equal(cIonian);
 	
-			expect(Scale.create('', 'harmonic', 'g-flat')).to.deep.equal(gFlatHarmonicMinor);
-			expect(Scale.create('Minor', 'Harmonic', 'G-FLAT')).to.deep.equal(gFlatHarmonicMinor);
+			expect(Scale.create('', 'harmonic', 'b-flat')).to.deep.equal(bFlatHarmonicMinor);
+			expect(Scale.create('Minor', 'Harmonic', 'B-FLAT')).to.deep.equal(bFlatHarmonicMinor);
 		});
 
 	});
--- tests/unit/music-theory/scale.spec.ts
+++ tests/unit/music-theory/scale.spec.ts
@@ -1,27 +1,33 @@
 import { Scale as ScaleModel } from '@/models/scale';
-import { getScaleInfo, getScaleTriadNames } from '@/music-theory/scale';
+import { getScaleInfo, getScaleTriadNames, relativeTonicInIonian } from '@/music-theory/scale';
 import { expect } from 'chai';
 
 describe('music-theory/scale', () => {
 
 	it('getScale', () => {
-		expect(getScaleInfo(ScaleModel.create('major', 'ionian', 'c')).name).to.equal('C major');
-		expect(getScaleInfo(ScaleModel.create('major', 'dorian', 'c')).name).to.equal('C dorian');
-		expect(getScaleInfo(ScaleModel.create('major', 'phrygian', 'c')).name).to.equal('C phrygian');
-		expect(getScaleInfo(ScaleModel.create('major', 'lydian', 'c')).name).to.equal('C lydian');
-		expect(getScaleInfo(ScaleModel.create('major', 'mixolydian', 'c')).name).to.equal('C mixolydian');
-		expect(getScaleInfo(ScaleModel.create('major', 'aeolian', 'c')).name).to.equal('C aeolian');
-		expect(getScaleInfo(ScaleModel.create('major', 'locrian', 'c')).name).to.equal('C locrian');
-		expect(getScaleInfo(ScaleModel.create('minor', 'natural', 'c')).name).to.equal('C aeolian');
-		expect(getScaleInfo(ScaleModel.create('minor', 'harmonic', 'c')).name).to.equal('C harmonic minor');
-		expect(getScaleInfo(ScaleModel.create('minor', 'melodic', 'c')).name).to.equal('C melodic minor');
+		expect(getScaleInfo(new ScaleModel('major', 'ionian', 'c')).name).to.equal('C major');
+		expect(getScaleInfo(new ScaleModel('major', 'dorian', 'c')).name).to.equal('C dorian');
+		expect(getScaleInfo(new ScaleModel('major', 'phrygian', 'c')).name).to.equal('C phrygian');
+		expect(getScaleInfo(new ScaleModel('major', 'lydian', 'c')).name).to.equal('C lydian');
+		expect(getScaleInfo(new ScaleModel('major', 'mixolydian', 'c')).name).to.equal('C mixolydian');
+		expect(getScaleInfo(new ScaleModel('major', 'aeolian', 'c')).name).to.equal('C aeolian');
+		expect(getScaleInfo(new ScaleModel('major', 'locrian', 'c')).name).to.equal('C locrian');
+		expect(getScaleInfo(new ScaleModel('minor', 'natural', 'c')).name).to.equal('C aeolian');
+		expect(getScaleInfo(new ScaleModel('minor', 'harmonic', 'c')).name).to.equal('C harmonic minor');
+		expect(getScaleInfo(new ScaleModel('minor', 'melodic', 'c')).name).to.equal('C melodic minor');
 	});
 
 	it('getScaleTriadNames', () => {
-		expect(getScaleTriadNames(ScaleModel.create('major', 'ionian', 'c'))).to.deep.equal(['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim']);
-		expect(getScaleTriadNames(ScaleModel.create('minor', 'natural', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Eb', 'Fm', 'Gm', 'Ab', 'Bb']);
-		expect(getScaleTriadNames(ScaleModel.create('minor', 'harmonic', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Ebaug', 'Fm', 'G', 'Ab', 'Bdim']);
-		expect(getScaleTriadNames(ScaleModel.create('minor', 'melodic', 'c'))).to.deep.equal(['Cm', 'Dm', 'Ebaug', 'F', 'G', 'Adim', 'Bdim']);
+		expect(getScaleTriadNames(new ScaleModel('major', 'ionian', 'c'))).to.deep.equal(['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim']);
+		expect(getScaleTriadNames(new ScaleModel('minor', 'natural', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Eb', 'Fm', 'Gm', 'Ab', 'Bb']);
+		expect(getScaleTriadNames(new ScaleModel('minor', 'harmonic', 'c'))).to.deep.equal(['Cm', 'Ddim', 'Ebaug', 'Fm', 'G', 'Ab', 'Bdim']);
+		expect(getScaleTriadNames(new ScaleModel('minor', 'melodic', 'c'))).to.deep.equal(['Cm', 'Dm', 'Ebaug', 'F', 'G', 'Adim', 'Bdim']);
+	});
+
+	it('relativeTonicInIonian', () => {
+		expect(relativeTonicInIonian(new ScaleModel('major', 'lydian', 'd'))).to.equal('a');
+		expect(relativeTonicInIonian(new ScaleModel('minor', 'natural', 'b-flat'))).to.equal('d-flat');
+		expect(relativeTonicInIonian(new ScaleModel('minor', 'melodic', 'a'))).to.equal('c');
 	});
 
 });
