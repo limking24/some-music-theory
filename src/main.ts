5700eaeb178334418037cf4a208fa72e2b71099b
kinglim3 <limkingfai@gmail.com>
2021-08-26 23:12:23 +0800

    Remove unused component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { Container } from 'typescript-ioc';
 import { createApp } from 'vue';
-import App from './app.vue';
+import App from './App.vue';
 import serviceConfig from './config/service.config';
 import router from './router';
 
