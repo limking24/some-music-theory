0437651e5e2cbb38607d53f5cc355f3f0b54590e
kinglim3 <limkingfai@gmail.com>
2021-09-05 19:36:02 +0800

    Refactor code

--- src/models/scale-name.ts
+++ src/models/scale-name.ts
@@ -1,27 +1,62 @@
 import { toTitleCase } from '@/functional/string';
 import { ScaleType } from '@tonaljs/tonal';
 
+const replacement = {
+	' ': '-',
+	'\'': '',
+	'#': ''
+} as {
+	[key: string]: string
+};
+
 export class ScaleName {
 
-	public constructor(public key: string, public display: string, public aliases?: string[]) {}
-
-}
-
-export interface KeyedScaleName {
-	[key: string]: ScaleName;
-}
-
-export const ScaleNameMap = ScaleType
+	private static MAP = ScaleType
 							.all()
 							.filter(scale => scale.name !== 'chromatic')
-							.reduce((unsorted, scale) => {
-								unsorted[scale.name] = new ScaleName(scale.name, toTitleCase(scale.name), scale.aliases);
-								scale.aliases.forEach(alias => 
-									unsorted[alias] = new ScaleName(scale.name, toTitleCase(alias[0] === '·' ? alias.slice(1) : alias), scale.aliases.map(a => (a == alias) ? scale.name : a))
-								);
-								return unsorted;
-							}, {} as KeyedScaleName);
+							.reduce((map, scale) => {
+								/* e.g.
+								   names: ['half-whole diminished', 'dominant diminished', 'messiaen's mode #2']
+								   keys:  ['half-whole-diminished', 'dominant-diminished', 'messiaens-mode-2'] */
+								let names = [ scale.name, ...scale.aliases.map(alias => alias.startsWith('·') ? alias.slice(1) : alias)];
+								let keys = names.map(name => name.replace(/['# ]/g, char => replacement[char]));
+								keys.forEach((key, index) => {
+									map[key] = new ScaleName(
+										key, 
+										scale.name, 
+										toTitleCase(names[index]), 
+										keys.filter(k => k !== key)
+									);
+								});
+								return map;
+							}, {} as {
+								[key: string]: ScaleName;
+							});
 
-export const SortedScaleNameKey = Object
-									.keys(ScaleNameMap)
-									.sort((a, b) => ScaleNameMap[a].display.localeCompare(ScaleNameMap[b].display));
+	public static KEYS = Object
+							.keys(ScaleName.MAP)
+							.sort((a, b) => ScaleName.MAP[a].display.localeCompare(ScaleName.MAP[b].display));
+
+	public constructor(public readonly key: string,
+						public readonly ref: string,
+						public readonly display: string,
+						public readonly aliasKeys: string[]) {}
+
+	public static get(key: string, defaultKey = 'major'): ScaleName {
+		let scaleName = ScaleName.MAP[key];
+		if (scaleName === undefined) {
+			scaleName = ScaleName.MAP[defaultKey];
+		}
+		return scaleName!;
+	}
+
+	public static aliasKeysOf(key: string): string[] {
+		let scaleName = ScaleName.MAP[key];
+		return scaleName ? scaleName.aliasKeys : [];
+	}
+
+	public get aliases(): ScaleName[] {
+		return this.aliasKeys.map(key => ScaleName.MAP[key]);
+	}
+
+}
