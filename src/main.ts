aff8c5134d40ceb60dbdeaa3ab2641d9a3843ab1
kinglim3 <limkingfai@gmail.com>
2021-09-08 00:46:48 +0800

    Rename config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import 'reflect-metadata';
 import { Container } from 'typescript-ioc';
 import { createApp } from 'vue';
 import App from './app.vue';
-import scaleNameConfig from './config/scale-name.config';
+import scaleDataConfig from './config/scale-data.config';
 import serviceConfig from './config/service.config';
 import router from './router';
 
-Container.configure(...scaleNameConfig, ...serviceConfig);
+Container.configure(...scaleDataConfig, ...serviceConfig);
 createApp(App).use(router).mount('#app')
