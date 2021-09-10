f7006e5ae5e5336641abfe8a5967750cff65d346
Lim King Fai <king-fai.lim@varmeego.com>
2021-09-10 18:39:22 +0800

    Add ScaleNotesTableRowFactory

---
+++ src/config/factories-config.ts
@@ -0,0 +1,5 @@
+import { ScaleNotesTableRowFactory, TonalScaleNotesTableRowFactory } from '@/factories/scale-notes-table-row-factory';
+
+export default [
+	{ bind: ScaleNotesTableRowFactory, to: TonalScaleNotesTableRowFactory }
+];
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { Container } from 'typescript-ioc';
 import { createApp } from 'vue';
 import App from './app.vue';
 import dataAccessConfig from './config/data-access-config';
+import factoriesConfig from './config/factories-config';
 import serviceConfig from './config/service.config';
 import router from './router';
 
-Container.configure(...dataAccessConfig, ...serviceConfig);
+Container.configure(...dataAccessConfig, ...factoriesConfig, ...serviceConfig);
 createApp(App).use(router).mount('#app')
