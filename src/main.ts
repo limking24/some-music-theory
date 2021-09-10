e475415e8fad9ca06229fb5a44c0a750fe1e0169
Lim King Fai <king-fai.lim@varmeego.com>
2021-09-10 16:21:57 +0800

    Update config setting

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import 'reflect-metadata';
 import { Container } from 'typescript-ioc';
 import { createApp } from 'vue';
 import App from './app.vue';
-import scaleDataConfig from './config/scale-data.config';
+import dataAccessConfig from './config/data-access-config';
 import serviceConfig from './config/service.config';
 import router from './router';
 
-Container.configure(...scaleDataConfig, ...serviceConfig);
+Container.configure(...dataAccessConfig, ...serviceConfig);
 createApp(App).use(router).mount('#app')
