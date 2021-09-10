01273667644036451fff803005e70d263fceea1d
kinglim3 <limkingfai@gmail.com>
2021-09-10 23:58:58 +0800

    Remove abstract class

--- src/factories/scale-notes-table-info-factory.ts
+++ src/factories/scale-notes-table-info-factory.ts
@@ -4,26 +4,17 @@ import { ScaleNotesTableInfo } from '@/models/scale-notes-table-info';
 import { Inject, Singleton } from 'typescript-ioc';
 import { ScaleNotesTableRowFactory } from './scale-notes-table-row-factory';
 
-export abstract class ScaleNotesTableInfoFactory {
-	
-	public abstract create(scaleName: ScaleName): ScaleNotesTableInfo;
-
-}
-
 @Singleton
-export class TonalScaleNotesTableFactory extends ScaleNotesTableInfoFactory {
+export class ScaleNotesTableInfoFactory {
 
 	public constructor(@Inject private _nameDao: ScaleNameDao,
-						@Inject private _rowFactory: ScaleNotesTableRowFactory) {
-		super();
-	}
+						@Inject private _rowFactory: ScaleNotesTableRowFactory) {}
 
 	public create(scaleName: ScaleName): ScaleNotesTableInfo {
-		let rows = this._rowFactory.create(scaleName.key);
 		return new ScaleNotesTableInfo(
 			scaleName,
 			this._nameDao.aliasesOf(scaleName.key),
-			rows
+			this._rowFactory.create(scaleName.key)
 		);
 	}
 	
