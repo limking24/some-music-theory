e93d4cd2d09821442b2748a8943a7aae13080989
kinglim3 <limkingfai@gmail.com>
2021-09-14 22:56:55 +0800

    Update scale dao

--- src/data-access/scale-dao.ts
+++ src/data-access/scale-dao.ts
@@ -1,14 +1,16 @@
 import { ScaleType } from '@/data/scale-type';
 import { Inject, Singleton } from 'typescript-ioc';
+import { Optional } from 'typescript-optional';
 import { SMTDB } from './database';
 
 export abstract class ScaleDao {
 
-	// public abstract nonChromaticNames(): Promise<ScaleName[]>;
+	public abstract get(key: string): Promise<Optional<ScaleType>>;
 
-	public abstract get(key: string): Promise<ScaleType>;
+	public abstract getBySupertype(supertype: number): Promise<ScaleType[]>;
 
 	public abstract supertypes(): Promise<number[]>;
+
 
 }
 
@@ -19,25 +21,18 @@ export class DexieScaleDao extends ScaleDao {
 		super();
 	}
 
-	public get(key: string): Promise<ScaleType> {
-		return {} as Promise<ScaleType>;
+	public async get(key: string): Promise<Optional<ScaleType>> {
+		let type = await this._db.scaleTypes.where('key').equalsIgnoreCase(key).first();
+		return Optional.ofNullable(type);
+	}
+
+	public async getBySupertype(supertype: number): Promise<ScaleType[]> {
+		return await this._db.scaleTypes.where('supertype').equals(supertype).toArray();
 	}
 
 	public async supertypes(): Promise<number[]> {
 		const array = await this._db.scaleTypes.orderBy('supertype').uniqueKeys();
 		return array.map(part => Number(part));
 	}
-
-	// public nonChromaticNames(): Promise<ScaleName[]> {
-	// 	let names = new Array<ScaleName>();
-	// 	return this._db.scaleTypes
-	// 					.where('notesPerOctave').below(12)
-	// 					.each(scale => names.push({
-	// 						key: scale.key,
-	// 						display: scale.display,
-	// 						aliases: scale.aliases 
-	// 					}))
-	// 					.then(() => names);
-	// }
 
 }
