487f8fcf0c1ca7459339d0699bae1ea85d252e4c
kinglim3 <limkingfai@gmail.com>
2021-09-14 22:57:18 +0800

    Add a service

--- src/config/services.config.ts
+++ src/config/services.config.ts
@@ -1,7 +1,8 @@
 import { HighlightableVexFlowScaleTriadsScoreDrawer, ScaleTriadsScoreDrawer } from '@/services/scale-triads-score-drawer';
+import { DbBasedScaleTypePickerOptionProvider, ScaleTypePickerOptionProvider } from '@/services/scale-type-picker-option-provider';
 
 export default [
-	// Scale Triads Score
+	{ bind: ScaleTypePickerOptionProvider, to: DbBasedScaleTypePickerOptionProvider },
 	{ bind: ScaleTriadsScoreDrawer, to: HighlightableVexFlowScaleTriadsScoreDrawer },
 	{ bindName: 'scale.triads.score.element.id', to: 'vexflow-scale-triads-score' },
 	{ bindName: 'scale.triads.score.width', to: 765 }
