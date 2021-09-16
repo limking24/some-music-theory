65e2f8b2e6988469fe0059b0cda1f4c9f59fe0e6
kinglim3 <limkingfai@gmail.com>
2021-09-16 23:48:05 +0800

    Remove service

--- src/config/services.config.ts
+++ src/config/services.config.ts
@@ -1,8 +1,6 @@
 import { HighlightableVexFlowScaleTriadsScoreDrawer, ScaleTriadsScoreDrawer } from '@/services/scale-triads-score-drawer';
-import { DbBasedScaleTypePickerOptionProvider, ScaleTypePickerOptionProvider } from '@/services/scale-type-picker-option-provider';
 
 export default [
-	{ bind: ScaleTypePickerOptionProvider, to: DbBasedScaleTypePickerOptionProvider },
 	{ bind: ScaleTriadsScoreDrawer, to: HighlightableVexFlowScaleTriadsScoreDrawer },
 	{ bindName: 'scale.triads.score.element.id', to: 'vexflow-scale-triads-score' },
 	{ bindName: 'scale.triads.score.width', to: 765 }
