380963aa56d211e38ed94403ec93b0021a86dda3
kinglim3 <limkingfai@gmail.com>
2021-09-18 18:17:10 +0800

    Change ScaleTonicRange to an interface

--- src/data/scale-tonic-range.ts
+++ src/data/scale-tonic-range.ts
@@ -1,10 +1,4 @@
-export class ScaleTonicRange {
-
-	public constructor(public readonly upper: number,
-						public readonly lower: number) {}
-
-	public outOfRange(index: number): boolean {
-		return index < this.upper || index > this.lower;
-	}
-
+export interface ScaleTonicRange {
+	readonly upper: number;
+	readonly lower: number;
 }
